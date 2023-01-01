5a00791cf34d7c1ca2ba6feb22dde7379a3f651c
LeVanHang <vanhang1802@gmail.com>
2023-01-01 15:55:46 +0700

    java home

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.printf(i + "\t");
+        }
+        System.out.printf("\n");
+
+       String[] cars = {"vinfat","honda"};
+        for (String i: cars
+             ) {
+            System.out.printf(i +"\t");
+        }
+        System.out.println();
+        String name = "Hang";
+        System.out.println("halo " + name);
+
+        int x= 2, y = 3, z = 4;
+        int sum;
+        sum = x + y +z;
+        System.out.printf("sum = " + sum);
     }
+
+
+
+
 }
