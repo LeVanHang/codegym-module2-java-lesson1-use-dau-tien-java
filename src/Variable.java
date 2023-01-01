518c39fb850a6f617c528a5d68ad4d77d348388f
LeVanHang <vanhang1802@gmail.com>
2023-01-01 16:21:56 +0700

    Area done

---
+++ src/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 10f;
+        double d = 10;
+        boolean b = true;
+        char c = 'c';
+        String s = "HN";
+
+        System.out.printf("i = " +i);
+        System.out.println();
+        System.out.printf("f = " +f);
+        System.out.println();
+        System.out.printf("d = " +d);
+        System.out.println();
+        System.out.printf("b = " +b);
+        System.out.println();
+        System.out.printf("c = " +c);
+        System.out.println();
+        System.out.printf("s = " + s);
+    }
+}
